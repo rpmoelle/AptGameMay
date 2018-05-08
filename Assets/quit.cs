f1c3a659673ec0715260b509abe498f3cb36030b
Rachel Moeller <parrishwork@gmail.com>
2018-05-07 23:52:04 -0400

    Emergency fix

--- Assets/quit.cs
+++ Assets/quit.cs
@@ -11,6 +11,9 @@ public class quit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
 }

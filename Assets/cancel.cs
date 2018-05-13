c4d17148d5acfcdea2a672efb7cd0e5f6374242f
Rachel Moeller <parrishwork@gmail.com>
2018-05-13 10:57:59 -0400

    Cursor Lock bug fix

--- Assets/cancel.cs
+++ Assets/cancel.cs
@@ -7,8 +7,8 @@ public class cancel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Cursor.lockState = CursorLockMode.None;
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/quit.cs
+++ Assets/quit.cs
@@ -6,8 +6,8 @@ public class quit : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Cursor.lockState = CursorLockMode.None;
+    }
 	
 	// Update is called once per frame
 	void Update () {
